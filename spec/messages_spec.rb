2fad0d6240333b00da7299990957d81a19d2e50b
Elliot Olbright <elliotolbright@gmail.com>
2021-05-20 16:21:20 -0600

    fix message methods

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -1,41 +1,41 @@
 require 'rspec'
-require './lib/command_prompts'
+require './lib/messages'
 
 RSpec.describe do
     it 'exists' do
-      prompts = Prompts.new
+      message = Message.new
       
-      expect(prompts).to be_an_instance_of(Prompts)
+      expect(message).to be_an_instance_of(Message)
     end
 
     it 'has an intro' do
-        prompts = Prompts.new
+        message = Message.new
          
-        expect(prompts.game_intro).to eq("Welcome to Mastermind - would you like to (p)lay, (r)ead the instructions, or (q)uit?")
+        expect(message.game_intro).to eq("Welcome to Mastermind - would you like to (p)lay, (r)ead the instructions, or (q)uit?")
     end
 
     it 'has a bad intro choice' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.intro_bad_choice).to eq("That is not a valid choice. Please choose (p)play, (r)ead the instructions, or (q)uit")
+        expect(message.intro_bad_choice).to eq("That is not a valid choice. Please choose (p)play, (r)ead the instructions, or (q)uit")
     end
 
-    it 'prompts to play' do
-        prompts = Prompts.new
+    it 'message to play' do
+        message = Message.new
 
-        expect(prompts.play_prompts).to eq("I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, (y)ellow. Use (q) to quit at any time to end the game. What’s your guess?")
+        expect(message.play_message).to eq("I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, (y)ellow. Use (q) to quit at any time to end the game. What’s your guess?")
     end
 
     it 'confirms quit' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.quit_confirm).to eq("Ya sure?")
+        expect(message.quit_confirm).to eq("Ya sure?")
     end
 
     it 'has instructions' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.instructions).to eq("The computer has selected a      secret combination of 4 colors and you have to guess that combination in 10 or fewer tries to win.
+        expect(message.instructions).to eq("The computer has selected a      secret combination of 4 colors and you have to guess that combination in 10 or fewer tries to win.
 
         To create your guess, choose a combination of (r)ed, (g)reen, (b)lue, (y)ellow. **You can do doubles or triples** 
 
@@ -44,34 +44,34 @@ RSpec.describe do
     end 
 
     it 'has a cheat' do
-        prompts = Prompts.new
+        message = Message.new
 # fix string interpolation
-        expect(prompts.cheat).to eq("secret code is #{'secret_code'}")
+        expect(message.cheat).to eq("secret code is #{'secret_code'}")
     end 
 
     it 'has 4 characters' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.incorrect_length).to eq("please enter a valid code (4 characters)")
+        expect(message.incorrect_length).to eq("please enter a valid code (4 characters)")
     end 
 
     it 'has a color choice' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.incorrect_color).to eq("please enter a correct color choice using (r)ed, (g)reen, (b)lue, (y)ellow")
+        expect(message.incorrect_color).to eq("please enter a correct color choice using (r)ed, (g)reen, (b)lue, (y)ellow")
     end
 
-    it 'can guess' do
-        prompts = Prompts.new
+    it 'can guess' do #takes arguments
+        message = Message.new
 
-        expect(prompts.guess_prompt).to eq("#{'guessed_sequence'} has 
+        expect(message.guess_message).to eq("#{'guessed_sequence'} has 
         #{'number_of_correct_elements'} with #{'number_in_correct_position'} in the correct positions. You have taken #{'number_of_guesses'}. Please guess again:)")
     end 
 
     it 'it can win' do
-        prompts = Prompts.new
+        message = Message.new
 
-        expect(prompts.win_prompt).to eq("Congratulations! You guessed #{'secret_code'}, in #{'number_of_guesses'} over #{'total_time'}.  Would you like to (p)lay again, or (q)uit?")
+        expect(message.win_message).to eq("Congratulations! You guessed #{'secret_code'}, in #{'number_of_guesses'} over #{'total_time'}.  Would you like to (p)lay again, or (q)uit?")
     end 
 
 end
