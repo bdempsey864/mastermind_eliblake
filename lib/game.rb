7c3194ac17a67a5de1317467d11c18f018acbd6f
Blake Dempsey <bdempsey864@gmail.com>
2021-05-20 17:46:41 -0600

    Added start game

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,13 @@
+require './lib/messages'
+
 class Game 
+    def initialize
+        @message = Message.new
+    end
+
+    def start
+        puts @message.game_intro
+        
+    end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,17 @@
 require './lib/messages'
 require './lib/game'
 require 'rspec'
+
+RSpec.describe 'game' do
+    it 'is a game' do
+        game = Game.new
+
+        expect(game).to be_a(Game)
+    end 
+
+    it 'starts' do
+        game = Game.new
+
+        expect(game.start).to eq(puts @message.game_intro)
+    end
+end
